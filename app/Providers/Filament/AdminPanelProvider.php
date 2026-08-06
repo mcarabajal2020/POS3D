f343c0de4d309112397efd46b96e23bde9c620df
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2026-08-05 21:41:40 -0300

    feat: redirect to list view after creating a record
    Add ->resourceCreatePageRedirect('index') to panel config so all resources
    redirect to the list page after saving a new record instead of the edit page.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->resourceCreatePageRedirect('index')
             ->colors([
                 'primary' => Color::Amber,
             ])
