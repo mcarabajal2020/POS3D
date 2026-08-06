67abf368414706acda5e3d0641fa2d5aea6d1bfc
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2026-08-05 21:27:02 -0300

    feat: add mobile bottom nav and remove dashboard welcome page
    - Install hammadzafar05/mobile-bottom-nav plugin for mobile navigation
    - Remove Dashboard page and default Filament widgets from panel
    - Configure bottom nav with 4 items + 'Más' button

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -7,12 +7,10 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
+use Hammadzafar05\MobileBottomNav\MobileBottomNav;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
@@ -34,14 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
-            ->pages([
-                Dashboard::class,
-            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([
-                //  AccountWidget::class,
-                //  FilamentInfoWidget::class,
-            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -62,6 +53,11 @@ class AdminPanelProvider extends PanelProvider
                     ->dashboard()
                     ->documentation(false)
                     ->navigationGroup('Mercado Pago'),
+            )
+            ->plugin(
+                MobileBottomNav::make()
+                    ->fromNavigation(4)
+                    ->moreButtonLabel('Más'),
             );
     }
 }
