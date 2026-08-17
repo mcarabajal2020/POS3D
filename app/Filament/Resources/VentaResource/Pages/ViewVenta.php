2ff4a3c3592f185f1878d5377f1358503c0bebfa
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2026-08-17 09:43:19 -0300

    feat: agregar action para cambiar estado de venta desde la vista

--- app/Filament/Resources/VentaResource/Pages/ViewVenta.php
+++ app/Filament/Resources/VentaResource/Pages/ViewVenta.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Resources\VentaResource\Pages;
 
+use App\Enums\EstadoVenta;
 use App\Filament\Resources\VentaResource;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewVenta extends ViewRecord
@@ -11,6 +15,27 @@ class ViewVenta extends ViewRecord
 
     protected function getHeaderActions(): array
     {
-        return [];
+        return [
+            Action::make('cambiarEstado')
+                ->label('Cambiar estado')
+                ->icon('heroicon-o-arrow-path')
+                ->color('warning')
+                ->form([
+                    Select::make('estado')
+                        ->label('Estado')
+                        ->options(EstadoVenta::class)
+                        ->required()
+                        ->native(false)
+                        ->default(fn ($record) => $record->estado),
+                ])
+                ->action(function (array $data, $record) {
+                    $record->update(['estado' => $data['estado']]);
+
+                    Notification::make()
+                        ->title('Estado actualizado')
+                        ->success()
+                        ->send();
+                }),
+        ];
     }
 }
